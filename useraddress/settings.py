89c72a451061c3d163bccb5a65aae0b593d184ba
Aditi Shirsat <aditishirsat.magneto@gmail.com>
2020-08-02 15:16:39 +0530

    initial commit

--- useraddress/settings.py
+++ useraddress/settings.py
@@ -77,23 +77,23 @@ WSGI_APPLICATION = 'useraddress.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'mydb',
-        # 'USER': 'root',
-        # 'PASSWORD': 'Adi@12345',
-        # 'HOST': 'localhost',
-        # 'PORT': '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'mydb',
+#         # 'USER': 'root',
+#         # 'PASSWORD': 'Adi@12345',
+#         # 'HOST': 'localhost',
+#         # 'PORT': '3306',
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
